5af8817e2d177cf68e7a194606f06c4c7126c2e7
NickABN <115517667+NicolasAlejandroBasilioNava@users.noreply.github.com>
2023-11-08 18:15:45 -0600

    Modelos Proveedores, restaurantes, Clientes

--- clients/models.py
+++ clients/models.py
@@ -1,3 +1,24 @@
 from django.db import models
+from django.contrib.gis.db import models
 
 # Create your models here.
+class Proveedores(models.Model):
+    id_proveedor = models.SmallAutoField()
+    descripcion = models.CharField(max_length=400)
+
+    def __str__(self):
+        return self.descripcion
+    
+class Restaurantes:
+    id_restaurante = models.SmallAutoField()
+    descripcion = models.CharField(max_length=400)
+    location = models.PointField()
+
+    def __str__(self):
+        return self.descripcion
+    
+class Clientes:
+    id_cliente = models.SmallAutoField()
+
+    def __str__(self):
+        return self.id_cliente
