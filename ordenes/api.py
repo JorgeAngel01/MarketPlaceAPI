1679a9c7caffc908e21eebf5b504c442bf1cb1fb
LeiWithP <pedro.barrita1029@gmail.com>
2023-12-03 22:37:36 -0600

    new ordenes behavior

--- ordenes/api.py
+++ ordenes/api.py
@@ -16,60 +16,74 @@ class OrdenItemViewSet(viewsets.ModelViewSet):
 
     def create(self, request, *args, **kwargs):
         username = request.query_params.get('username')
-
         if 'orden' not in request.data or not request.data['orden']:
-            if username:
-                try:
-                    user = User.objects.get(username=username)
-                except User.DoesNotExist:
-                    return response.Response({'error': 'User not found'}, status=status.HTTP_404_NOT_FOUND)
-            else:
-                return response.Response({'error': 'Username is required'}, status=status.HTTP_400_BAD_REQUEST)
-            new_orden = Orden(cliente=user)
-            new_orden.save()
-            request.data['orden'] = new_orden.id
+            user = self.get_user_by_username(username) if username else None
+            if not user:
+                return response.Response({'error': 'User not found or username is required'}, status=status.HTTP_400_BAD_REQUEST)
+            request.data['orden'] = self.create_orden(user).id
 
-        # return super(OrdenItemViewSet, self).create(request, *args, **kwargs)
-        response = super(OrdenItemViewSet, self).create(request, *args, **kwargs)
-        self.update_orden_precio_total(response.data['orden'])
-        return response
-    
+        resp = super(OrdenItemViewSet, self).create(request, *args, **kwargs)
+        self.update_orden_precio_total(resp.data['orden'])
+        return resp
+
     def update(self, request, *args, **kwargs):
-        # Existing logic for updating an OrdenItem...
-        response = super(OrdenItemViewSet, self).update(request, *args, **kwargs)
-        self.update_orden_precio_total(response.data['orden'])
-        return response
+        resp = super(OrdenItemViewSet, self).update(request, *args, **kwargs)
+        orden_item = self.get_object()
+
+        # Update Orden's total price
+        self.update_orden_precio_total(orden_item.orden.id)
+
+        # Check and update Orden's estado if necessary
+        if orden_item.estado != 0:
+            self.check_and_update_orden_estado(orden_item.orden)
+
+        return resp
 
     def update_orden_precio_total(self, orden_id):
-        orden = Orden.objects.get(id=orden_id)
-        total_price = OrdenItem.objects.filter(orden=orden).annotate(
+        total_price = OrdenItem.objects.filter(orden_id=orden_id).annotate(
             item_total=F('cantidad') * F('producto__precio')
-        ).aggregate(Sum('item_total'))['item_total__sum'] or 0
-        orden.precio_total = total_price
-        orden.save()
+        ).aggregate(total=Sum('item_total'))['total'] or 0
+        Orden.objects.filter(id=orden_id).update(precio_total=total_price)
+
+    def check_and_update_orden_estado(self, orden):
+        orden_items = OrdenItem.objects.filter(orden=orden)
+        if not orden_items.filter(estado=0).exists():
+            Orden.objects.filter(id=orden.id).update(estado=4)
+
+    def update_orden_precio_total(self, orden_id):
+        total_price = OrdenItem.objects.filter(orden_id=orden_id).annotate(
+            item_total=F('cantidad') * F('producto__precio')
+        ).aggregate(total=Sum('item_total'))['total'] or 0
+        Orden.objects.filter(id=orden_id).update(precio_total=total_price)
+
+    def get_user_by_username(self, username):
+        try:
+            return User.objects.get(username=username)
+        except User.DoesNotExist:
+            return None
+
+    def create_orden(self, user):
+        new_orden = Orden(cliente=user)
+        new_orden.save()
+        return new_orden
 
 class GetOrdenView(views.APIView):
 
     def get(self, request, username):
         try:
             cliente = User.objects.get(username=username)
-            
-            latest = request.query_params.get('latest', 'false').lower() == 'true'
-
-            if latest:
-                orden = Orden.objects.filter(cliente=cliente).order_by('-fecha').first()
-                if orden:
-                    serializer = OrdenSerializer(orden)
-                else:
-                    return response.Response({'message': 'No orders found'}, status=status.HTTP_404_NOT_FOUND)
-            else:
-                ordenes = Orden.objects.filter(cliente=cliente).order_by('-fecha')
-                serializer = OrdenSerializer(ordenes, many=True)
-
-            return response.Response(serializer.data)
-
         except User.DoesNotExist:
             return response.Response({'error': 'User not found'}, status=status.HTTP_404_NOT_FOUND)
+
+        latest = request.query_params.get('latest', 'false').lower() == 'true'
+        ordenes = Orden.objects.filter(cliente=cliente).order_by('-fecha')
+        orden = ordenes.first() if latest and ordenes.exists() else None
+
+        if latest and not orden:
+            return response.Response({'message': 'No orders found'}, status=status.HTTP_404_NOT_FOUND)
+        
+        serializer = OrdenSerializer(orden if latest else ordenes, many=not latest)
+        return response.Response(serializer.data)
 
 class GetItemsOrdenView(views.APIView):
 
@@ -78,14 +92,40 @@ class GetItemsOrdenView(views.APIView):
         if not orden_id:
             return response.Response({'error': 'Orden ID is required'}, status=status.HTTP_400_BAD_REQUEST)
 
-        try:
-            # Verify if the Orden exists
-            Orden.objects.get(id=orden_id)
-        except Orden.DoesNotExist:
+        if not Orden.objects.filter(id=orden_id).exists():
             return response.Response({'error': 'Orden not found'}, status=status.HTTP_404_NOT_FOUND)
 
-        # Fetch all items related to the Orden
         orden_items = OrdenItem.objects.filter(orden_id=orden_id)
         serializer = OrdenItemSerializer(orden_items, many=True)
 
         return response.Response(serializer.data)
+
+class GetOrdenItemsByRestauranteProveedorView(views.APIView):
+
+    def get(self, request):
+        restaurante_id = request.query_params.get('restaurante')
+        proveedor_id = request.query_params.get('proveedor')
+
+        if restaurante_id:
+            orden_items = self.get_orden_items_by_restaurante(restaurante_id)
+        elif proveedor_id:
+            orden_items = self.get_orden_items_by_proveedor(proveedor_id)
+        else:
+            return response.Response({'error': 'Restaurante or Proveedor ID is required'}, status=status.HTTP_400_BAD_REQUEST)
+
+        serializer = OrdenItemSerializer(orden_items, many=True)
+        return response.Response(serializer.data)
+
+    def get_orden_items_by_restaurante(self, restaurante_id):
+        try:
+            restaurante_id = int(restaurante_id)
+            return OrdenItem.objects.filter(producto__restaurantes__id=restaurante_id)
+        except ValueError:
+            return OrdenItem.objects.none()
+
+    def get_orden_items_by_proveedor(self, proveedor_id):
+        try:
+            proveedor_id = int(proveedor_id)
+            return OrdenItem.objects.filter(producto__proveedores__id=proveedor_id)
+        except ValueError:
+            return OrdenItem.objects.none()
--- ordenes/models.py
+++ ordenes/models.py
@@ -24,9 +24,20 @@ class Orden(models.Model):
         return f"Orden de {self.cliente} realizada el {self.fecha.date()} a las {self.fecha.hour}:{self.fecha.minute}"
 
 class OrdenItem(models.Model):
+    ESTADO = [ 
+        ('0', 'Procesando'),
+        ('1', 'Cancelado'),
+        ('2', 'Aceptado'),
+    ]
+    
     orden = models.ForeignKey(Orden, related_name='items', on_delete=models.CASCADE)
     producto = models.ForeignKey(Producto, on_delete=models.CASCADE)
     cantidad = models.IntegerField(default=1)
+    estado = models.CharField(
+        max_length=2,
+        choices=ESTADO,
+        default=0,
+    )
     
     def __str__(self):
         return f"Producto {self.producto} en {self.orden}"
